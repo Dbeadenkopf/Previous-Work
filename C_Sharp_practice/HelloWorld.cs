705b25c86bdd5d8617cf7388d37dd6f416664d2e
David Beadenkopf <davidbeadenkopf@Davids-MacBook-Air.local>
2024-02-15 20:53:33 -0500

    initial commit

---
+++ C_Sharp_practice/HelloWorld.cs
@@ -0,0 +1,10 @@
+// lets program hello world in c sharp
+using SYSTEM;
+
+namespace HelloWorld{
+    class Program{
+        static void Main(string[] args){
+            SYSTEM.Console.WriteLine("Hello World Test");
+        }
+    }
+}
